8854bbad5422c98475067317d6e5a7b3bcff31cb
lisofoxa <lisofoxa@gmail.com>
2026-01-30 09:59:07 +0300

    Обновление.

--- script.js
+++ script.js
@@ -252,8 +252,8 @@ class MoodFlow {
             // Show emoji cursor (slightly offset to top-right so it doesn't cover the pointer)
             this.emojiCursor.textContent = emoji;
             this.emojiCursor.style.opacity = '1';
-            this.emojiCursor.style.left = `${e.clientX + 5}px`;
-            this.emojiCursor.style.top = `${e.clientY - 5}px`;
+            this.emojiCursor.style.left = `${e.clientX - 5}px`;
+            this.emojiCursor.style.top = `${e.clientY + 5}px`;
             
             // Hide default cursor on canvas
             e.target.style.cursor = 'none';
